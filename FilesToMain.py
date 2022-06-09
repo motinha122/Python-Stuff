935d376992f8b1654773d45794dc5dbfed8a2eb3
motinha122 <timota132@gmail.com>
2022-06-09 19:31:15 -0300

    Files to Main bug fixed

--- FilesToMain.py
+++ FilesToMain.py
@@ -13,18 +13,19 @@ print('''
     ''')
 
 mainFolder = os.listdir(os.getcwd())
-folders = []
+folders = [] 
+
 
 def setRootFoolder():
     for folder in mainFolder:
         if os.path.isdir(folder) and folder != '.git':
             folders.append(folder)
-            print(f'{folder.index(folder)}:{folder}')
+            print(f'{folders.index(folder)}:{folder}')
     if len(folders) != 0:
         mainDir = int(input("\nWhich folder : "))
         os.chdir(folders[mainDir])
         mainRoot = os.getcwd()
-        for path,files in os.walk(os.getcwd()):
+        for path,root,files in os.walk(os.getcwd()):
             for file in files:
                 if not os.path.exists(os.path.join(mainRoot,file)):
                     shutil.move(os.path.join(path,file),mainRoot)
