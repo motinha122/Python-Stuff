d277549cab26a8be170f1ef7b7c5e49d201cd1a5
motinha122 <timota132@gmail.com>
2022-06-09 18:38:14 -0300

    Files to Main finished

--- FilesToMain.py
+++ FilesToMain.py
@@ -1,3 +1,5 @@
+from msilib import _directories
+from msilib.schema import Directory
 import os 
 import shutil 
 
@@ -10,8 +12,28 @@ print('''
  |_|    |_|_|\___||___/  \__\___/  |_|  |_|\__,_|_|_| |_| 
     ''')
 
+mainFolder = os.listdir(os.getcwd())
+folders = []
+
+def setRootFoolder():
+    for folder in mainFolder:
+        if os.path.isdir(folder) and folder != '.git':
+            folders.append(folder)
+            print(f'{folder.index(folder)}:{folder}')
+    if len(folders) != 0:
+        mainDir = int(input("\nWhich folder : "))
+        os.chdir(folders[mainDir])
+        mainRoot = os.getcwd()
+        for path,files in os.walk(os.getcwd()):
+            for file in files:
+                if not os.path.exists(os.path.join(mainRoot,file)):
+                    shutil.move(os.path.join(path,file),mainRoot)
+    else:
+        return False
+
 def main():
-    print("Nyan Hello")
+    if setRootFoolder() == False:
+        print('Theres no directories')
 
 if __name__ == "__main__":
     main()
