34ce0941277a95f163ac379d9c4eb1e940df9c6a
motinha122 <timota132@gmail.com>
2022-06-12 09:12:18 -0300

    JQuery script added

--- HTML_Creator.py
+++ HTML_Creator.py
@@ -21,6 +21,8 @@ html = '''<!DOCTYPE html>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
+    <!--JQuery-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <!--CSS-->
     <link rel="Stylesheet" href="Css/sass.css">
     <link rel="icon" href="/images/favicon.ico">
